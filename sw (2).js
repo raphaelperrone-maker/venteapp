c93d6f5cfce318424e83f08e2098a98458d36dd3
raphaelperrone-maker <raphael.perrone@hotmail.fr>
2026-04-10 10:57:15 +0200

    Add files via upload

---
+++ sw (2).js
@@ -0,0 +1,30 @@
+const CACHE = 'allinone-v2';
+const BASE = 'https://raphaelperrone-maker.github.io/venteapp/';
+const ASSETS = [
+  BASE + 'app_vente.html',
+  BASE + 'manifest.json',
+  BASE + 'icon-192.png',
+  BASE + 'icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
